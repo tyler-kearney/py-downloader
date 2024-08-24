b402e47379466d820f55cebd10a4f231143a2fc4
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-08-24 15:58:26 -0500

    code commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # py-downloader
 A file downloader implemented with Python and Selenium
+
+For the project to work, you will need to install a web engine. The one I am testing with is the Chrome Web Engine
+Currently the path is pointed to a file on my device, you would need to change the path to where you download the file. 
+I do eventually plan to add a way to select the path yourself for the web driver. 
+
+The Gecko Web Engine (Based on Firefox) support is planned, but not implemented yet. 
---
+++ downloader.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import requests
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def download_file(url):
+    driver = webdriver.Chrome('/Users/tylerkearney/Documents/chromedriver-mac-arm64/chromedriver') 
+    # Eventually make a way for the user to set the path to their own web driver
+    driver.get(url)
+    
+    # Find the download button
+    download_btn = driver.find_element(By.XPATH, "//button[text()='Download']")
+    download_btn.click()
+    
+    driver.quit()
+    
+def start_download():
+    url = url_entry.get()
+    download_file(url)
+    
+# Create the window
+window = tk.Tk()
+window.title('PyDownloader')
+
+# Create a label and entry for the URL
+url_label = tk.Label(window, text="Enter URL: ")
+url_label.pack()
+
+url_entry = tk.Entry(window)
+url_entry.pack()
+
+# Create button to start the download
+download_btn = tk.Button(window, text='Download', command=start_download)
+download_btn.pack()
+
+window.mainloop()
